617d350824917fee0f572259cc4d116e39b320f7
Emracabe <caberemmar@gmail.com>
2022-09-28 18:19:29 +0800

    Remove the testing of the database

--- src/session2/Session2.java
+++ src/session2/Session2.java
@@ -15,30 +15,5 @@ public class Session2 {
      */
     public static void main(String[] args) {
       new Login();
-      printAllEmployeesFromDB();
-    }
-    
-    private static  void printAllEmployeesFromDB() {
-        try {
-            String url = "jdbc:mysql://localhost/session2";
-            String uname = "root";
-            String pass = "";
-            
-            String query = "SELECT * FROM employees";
-            
-            Statement st = DBConnect.getConn().createStatement();
-            
-            ResultSet rs = st.executeQuery(query);
-            
-            while(rs.next()) {
-                System.out.println(rs.getString("FirstName") + " " + rs.getString("LastName"));
-            }
-            
-            st.close();
-            rs.close();
-            
-        }  catch (SQLException ex) {
-            Logger.getLogger(Session2.class.getName()).log(Level.SEVERE, null, ex);
-        }
     }
 }
